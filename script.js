283089ad2a5cae7bcda8d5d954816a452af7e50e
Alice Minaif <alicesminaif.senai@gmail.com>
2026-08-16 22:43:34 -0300

    Add new CSS styles for writing blocks and implement video modal functions
    - Created `writing-block-dextp64a.css` for custom slider input styles.
    - Created `writing-block-provider-ne04av0z.css` for enhanced markdown editor styles.
    - Added `script.js` with functions to open and close a video modal.

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirVideo() {
+    const modal = document.getElementById("modal-video"); 
+    modal.style.display = "flex";
+}
+
+function fecharVideo() {
+    const modal = document.getElementById("modal-video");
+    modal.style.display = "none"
+}
